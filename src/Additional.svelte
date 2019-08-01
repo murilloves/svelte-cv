d565da5176c42c2f9784d6e00d80e895c4ed8a55
murilloves <murillo.vieiraesilva@gmail.com>
2019-08-01 00:43:07 -0300

    some education and certificates issues

--- src/Additional.svelte
+++ src/Additional.svelte
@@ -1,24 +1,16 @@
 <div class="section-summary">
-    <h3 class="subtitle">
-        ADDITIONAL INFORMATION
-    </h3>
 
-	<red>
-		<ul class="topic circle">
+	<h3 class="subtitle">
+        Additional Information
+    </h3>
+	<ul class="topic circle">
+		<red>
 			<li><strong>Tools and technology:</strong> MS Office, Photoshop, SAP</li>
 			<li><strong>Languages:</strong> French (Fluent) and German (Basic).</li>
-			<li><strong>Udemy Courses:</strong> Could be here or listed in Education.</li>
-			<li>React Native (30h).</li>
-			<li>MERN Stack (35h).</li>
-			<li>Advanced Angular (7h).</li>
-			<li> React Native with NodeJS and Firebase</li>
-			<li> MERN Stack, Front to Back</li>
-			<li> Agile management with scrum</li>
-			<li> Web Design for Web Developers: UI / UX for Websites</li>
-			<li> Javascript Completo 2018 - Fundamentals to Master</li>
-			<li>Burger lover and charismatic Pianist</li>
-		</ul>
-	</red>
+		</red>
+		<small>➤</small> Burger lover and charismatic Pianist
+	</ul>
+
 </div>
 
 <style>
@@ -32,4 +24,10 @@
 	}
 	li {word-wrap: break-word;}
 	red { color: red }
+	small {
+		color: #373737;
+		/* font-weight: bold;
+		font-size: 16px;
+		font-variant: diagonal-fractions; */
+	}
 </style>
--- src/Education.svelte
+++ src/Education.svelte
@@ -1,9 +1,23 @@
+<script>
+	let courses = [
+		{ name: 'React Native with NodeJS and Firebase', hours: 28 },
+		{ name: 'MERN Stack, Frontend to Backend', hours: 12 },
+		{ name: 'Agile management with SCRUM', hours: 10 },
+		{ name: 'Javascript Completo 2018 - Fundamentals to Master', hours: 35 },
+		{ name: 'Web Design for Web Developers: UI/UX for Websites and Mobile', hours: 8 }
+	]
+</script>
+
 <div class="section-summary">
     <h3 class="subtitle">
         EDUCATION
     </h3>
 
-    <div class="pb">
+    <h4>
+        University Degree
+    </h4>
+
+    <div class="pb ml-40">
         <row class="pb">
             <flex1>
                 2015 - 2018
@@ -32,6 +46,15 @@
             </flex3>
         </row>
     </div> -->
+
+    <h4>
+        Certificates
+    </h4>
+	<ul class="topic font-medium">
+		{#each courses as course, i}
+			{course.name} <medium>({course.hours}h)</medium>;<br />
+		{/each}
+	</ul>
 </div>
 
 
@@ -46,5 +69,25 @@
 	flex1 { flex: 1 }
 	flex3 { flex: 3 }
 
-	.pb { padding-bottom: 1rem }
+    .pb { padding-bottom: 1rem }
+    .ml-40 { margin-left: 40px }
+
+    h4 {
+        text-decoration: none;
+		font-size: 16px;
+        font-weight: 600;
+        margin-left: 20px;
+        margin-top: 5px;
+        margin-bottom: 10px;
+		/* text-transform: uppercase; */
+    	/* text-align: center; */
+	}
+	medium {
+        font-size: 12px;
+        font-style: italic
+    }
+	.font-medium {
+        font-size: 15px;
+        color: #444
+    }
 </style>
