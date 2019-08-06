eeba160b1d761e83566a326a793711df36df2238
murilloves <murillo.vieiraesilva@gmail.com>
2019-08-05 22:47:53 -0300

    CV done for now, improvements will come after working hard at Nectar CRM

--- src/Career.svelte
+++ src/Career.svelte
@@ -33,12 +33,14 @@
             : `${nectarInMonths % 12} mos` }
         </div>
 
-        <red>
+        <!-- WORK A BIT IN THIS PARAGRAPH AFTER -->
+        <!-- <red>
             <p>
-                made with React and Java Springboot Framework.
+                Nectar CRM is a startup located in Goiânia which provides solutions for companies for managing relationships with their customers, selling procedures, Business Intelligence and a lot more. Integrated with Google Calendar, RD Station and a huge set of enterprises...
+
                 During this time I was responsible for X,Y,Z. In order to accomplish this I had to make use of the coding languages A,B,C. I mainly focused on my skills with B, However during this job the combining of my skill and knowledge of these languages lead to to creating a better product. My most interesting project I've done at [company] is to create a [bla bla] that works with [bla bla]. I made sure to communicate frequently which helped decrease risk and allowed us to respond quickly to any unforseen errors.
             </p>
-        </red>
+        </red> -->
 
         <ul class="topic">
             <!-- <li>Highlight responsibilities and key achievements/deliverables.</li> -->
@@ -46,6 +48,7 @@
             <li>Shaped the CRM app with the new UX/UI set</li>
             <li>Improved the Backoffice webapp of the company</li>
             <li>Developed new functionalities into the software</li>
+            <!-- <li>React and Java Springboot Framework</li> -->
         </ul>
     </div>
     <div>
@@ -65,7 +68,7 @@
             Some projects I worked for Nussa Sistemas were: mobile app for package delivery, PWA for medical congresses, webapp with daily liturgy for Goiânia Archdiocese.
         </p>
     </div>
-    <div>
+    <div class="pt">
         <strong>Frontend Developer at Actuar</strong>
         <div class="tiny-desc">Leading provider of academic software in Brazil</div>
         <div>Jul 2017 – Dec 2018 • 1 yr 6 mos</div>
@@ -117,7 +120,8 @@
 	}
 	ul.upper-roman > li {
 		list-style-type: upper-roman;
-	} */
+    } */
+    .pt { padding-top: 2rem }
 	li {word-wrap: break-word;}
     red {color:red}
 </style>
